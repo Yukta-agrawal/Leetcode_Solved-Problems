880602e11ea3b4db7808c78390e0335da20209c1
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-05-22 14:20:55 +0530

    Time: 1 ms (74.34%), Space: 45.5 MB (55.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -18,9 +18,11 @@ class Solution {
         
         for(int i=1 ; i< m ; i++){
             for(int j=1 ; j< n ; j++){
+                if(matrix[i][j] != 0){
                 if(matrix[i][0] == 0 || matrix[0][j]==0){
                     matrix[i][j] = 0;
                 }
+            }
                
             }
         }
