1f038f1d79129d3d3490011864c59dab9fc85420
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-05-28 20:04:28 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (36.42%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,30 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer>  list = new ArrayList<Integer>() ;
-        helper(0 , nums , ans , list);
-        return ans ;
-        
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), result);
+        return result;
     }
     
-    
-    public void helper(int index ,int[] nums , List<List<Integer>> ans , List<Integer> list ){
-        
-        
-        if(index >= nums.length) {
-            ans.add(new ArrayList<>(list)) ;
-            return ;
+    private void backtrack(int[] nums, int start, List<Integer> path, List<List<Integer>> result) {
+        result.add(new ArrayList<>(path));
+        for (int i = start; i < nums.length; i++) {
+            path.add(nums[i]);
+            backtrack(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
         }
-
-        list.add(nums[index]);
-        
-        helper(index + 1 , nums , ans , list);
-        
-        list.remove(list.size() -1);
-        
-        helper(index + 1 , nums , ans , list);
- 
     }
-    
 }
