1797c7c52e2bb13bb28e89d8db21a7ea91477f57
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-05-30 17:21:29 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (6.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,32 +3,46 @@ class Solution {
         
         int[] dp = new int[nums.length];
         Arrays.fill(dp ,-1) ;
-        return helper(nums.length - 1 , nums , dp);
+        dp[0] = nums[0] ;
+        int take = Integer.MIN_VALUE ;
+        for(int i=1 ; i < nums.length ; i++){
+           if(i> 1){
+                take  = nums[i] + dp[i-2] ;
+           }
+            else{
+                take = nums[i];
+            }
+            int nottake = dp[i-1] ;
+            dp[i] = Math.max(take , nottake) ;
+        }
+        
+        
+        return dp[nums.length -1];
         
     }
     
     
-    public int helper(int index , int[] nums , int[] dp){
+//     public int helper(int index , int[] nums , int[] dp){
          
-        if(index ==0){
-            return nums[0] ;
-        }
+//         if(index ==0){
+//             return nums[0] ;
+//         }
         
-        if(index < 0){
-            return 0;
-        }
+//         if(index < 0){
+//             return 0;
+//         }
         
-        if(dp[index] != -1){
-            return dp[index] ;
-        } 
+//         if(dp[index] != -1){
+//             return dp[index] ;
+//         } 
         
         
-        int take = nums[index] + helper(index - 2 , nums , dp);
-        int nottake =0 + helper(index - 1 , nums , dp) ;
+//         int take = nums[index] + helper(index - 2 , nums , dp);
+//         int nottake =0 + helper(index - 1 , nums , dp) ;
         
-        dp[index] =  Math.max(take , nottake) ;
-        return dp[index] ;
+//         dp[index] =  Math.max(take , nottake) ;
+//         return dp[index] ;
         
-    }
+//     }
     
 }
