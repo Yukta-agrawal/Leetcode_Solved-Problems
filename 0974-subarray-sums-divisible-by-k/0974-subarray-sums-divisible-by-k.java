1bbf20181125482a83938d9b2e75546628b0caf0
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-06-09 15:53:09 +0530

    Time: 4 ms (89.40%), Space: 45.8 MB (89.57%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,21 +1,30 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
         int n = nums.length;
-        int prefixMod = 0, result = 0;
+        int sum = 0, count = 0;
 
         // There are k mod groups 0...k-1.
-        int[] modGroups = new int[k];
-        modGroups[0] = 1;
+        int[] map = new int[k];
+        Arrays.fill(map , 0) ;
+        map[0] = 1;
 
         for (int num: nums) {
-            // Take modulo twice to avoid negative remainders.
-            prefixMod = (prefixMod + num % k + k) % k;
-            // Add the count of subarrays that have the same remainder as the current
-            // one to cancel out the remainders.
-            result += modGroups[prefixMod];
-            modGroups[prefixMod]++;
+            sum = sum + num ;
+            int rem = sum % k;
+            
+            if(rem < 0){
+                rem = rem + k;
+            }
+            
+            if(map[rem] != 0){
+                count += map[rem];     
+            }
+            
+            map[rem] += 1;
+            
+            
         }
 
-        return result;
+        return count;
     }
 }
