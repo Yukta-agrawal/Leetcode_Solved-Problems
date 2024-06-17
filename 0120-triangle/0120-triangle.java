03c539f2a4f30d31f3249357b19d7d558dc46e93
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-06-17 14:22:14 +0530

    Time: 4 ms (48.13%), Space: 44 MB (47.76%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -8,27 +8,39 @@ class Solution {
         for(int[] row: dp){
             Arrays.fill(row, -1);
         }
-        return helper(triangle , 0 , 0 , m , dp);
-    
         
-        
-        
-    }
-    
-    public int helper(List<List<Integer>> triangle , int row , int col , int m , int[][] dp){
-        
-        if(row==m){
-            return 0;
+        for(int j = 0 ; j< m ; j++){
+            dp[m-1][j] = triangle.get(m-1).get(j);
         }
         
-        if(dp[row][col] != -1){
-            return dp[row][col];
+        for(int i=m-2 ; i>= 0 ; i--){
+            
+            for(int j=i; j>=0 ; j--){
+                
+                int sum1 = triangle.get(i).get(j) + dp[i+1][j];
+                int sum2 = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Math.min(sum1 , sum2) ;
+            }
+
         }
-        
-        int  sum1 = triangle.get(row).get(col) + helper(triangle , row+1 , col , m , dp);
-        int  sum2 = triangle.get(row).get(col) + helper(triangle , row+1 , col+1 , m , dp);
-        
-        return dp[row][col] = Math.min(sum1 , sum2);
-        
+      return dp[0][0];
+    
     }
+    
+//     public int helper(List<List<Integer>> triangle , int row , int col , int m , int[][] dp){
+        
+//         if(row==m){
+//             return 0;
+//         }
+        
+//         if(dp[row][col] != -1){
+//             return dp[row][col];
+//         }
+        
+//         int  sum1 = triangle.get(row).get(col) + helper(triangle , row+1 , col , m , dp);
+//         int  sum2 = triangle.get(row).get(col) + helper(triangle , row+1 , col+1 , m , dp);
+        
+//         return dp[row][col] = Math.min(sum1 , sum2);
+        
+//     }
 }
