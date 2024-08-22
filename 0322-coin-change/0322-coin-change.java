f234c768b92d6e6d0282fc4c1aac971e36dfe0ef
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-08-23 02:19:37 +0530

    Time: 16 ms (60.69%), Space: 45.4 MB (15.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -31,24 +31,4 @@ class Solution {
             return r;
     }
 
-    public int helper(int i , int[] coins, int amount ,int dp[][]){
-        if(i==0){
-            if(amount%coins[i]==0) return amount/coins[i];
-            else return (int)1e9;
-        }
-
-        if(dp[i][amount]!= -1){
-            return dp[i][amount] ;
-        }
-
-        int nottake = 0 + helper(i-1 , coins , amount , dp);
-        int take = Integer.MAX_VALUE;
-        if(amount-coins[i]>=0){
-            take = 1 + helper(i , coins , amount-coins[i] , dp);
-        }
-
-        return dp[i][amount] = Math.min(nottake , take);
-        //  dp[i][amount] ;
-
-    }
 }
