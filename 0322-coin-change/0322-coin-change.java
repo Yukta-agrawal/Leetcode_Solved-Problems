682901efe04d14fa8d1d83d321e574f6fc889051
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-08-23 02:19:03 +0530

    Time: 16 ms (60.69%), Space: 45 MB (25.13%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,16 +2,33 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
 
         int dp[][] = new int[coins.length][amount+1];
-        for(int[] row: dp){
-            Arrays.fill(row , -1);
+    
+        for(int a=0 ; a<= amount ; a++){
+            if(a % coins[0]==0) 
+                dp[0][a] = a/ coins[0] ;
+            else
+                dp[0][a] = (int)1e9 ;
         }
 
-        int r = helper(coins.length - 1 , coins , amount , dp);
+        for(int i=1 ; i<coins.length ; i++){
+            for(int a = 0; a<= amount ; a++){
+
+                int nottake = 0 + dp[i-1][a] ;
+                int take = Integer.MAX_VALUE;
+                if(a-coins[i]>=0){
+                    take = 1 + dp[i][a-coins[i]];
+                }
+                dp[i][a] = Math.min(nottake , take);
+            }
+        }
+
+
+        int r =  dp[coins.length - 1][amount];
         if(r== (int)1e9){
             return -1;
         }
         else
-         return r;
+            return r;
     }
 
     public int helper(int i , int[] coins, int amount ,int dp[][]){
