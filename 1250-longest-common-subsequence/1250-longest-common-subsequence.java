1863520c009975c546682e219a351668059d7150
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-08-25 18:08:47 +0530

    Time: 31 ms (12.4%), Space: 50.9 MB (32.84%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -2,17 +2,17 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n1 = text1.length();
         int n2 = text2.length();
-        int dp[][] = new int[n1][n2];
+        int dp[][] = new int[n1+1][n2+1];
         for(int[] row: dp){
             Arrays.fill(row , -2);
         }
-        return helper(n1-1 , n2-1 , text1 , text2 , dp);
+        return helper(n1 , n2 , text1 , text2 , dp);
 
     }
 
     public int helper(int ind1 , int ind2 , String text1, String text2 , int[][] dp){
 
-        if(ind1<0 || ind2<0){
+        if(ind1==0 || ind2==0){
             return 0;
         }
 
@@ -20,7 +20,7 @@ class Solution {
             return dp[ind1][ind2];
         }
 
-        if(text1.charAt(ind1)==text2.charAt(ind2)){
+        if(text1.charAt(ind1-1)==text2.charAt(ind2-1)){
             return dp[ind1][ind2] = 1 + helper(ind1-1 , ind2-1 , text1 , text2 , dp);
         }
 
