5543c6fc406d463d52ca4877e5e746699e63e17a
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-12-09 19:36:22 +0530

    Time: 1 ms (92.27%), Space: 45.9 MB (39.89%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int dp[][] = new int[n][2] ;
-        for(int[] d: dp){
-            Arrays.fill(d , -1);
+        int dp[][] = new int[n+1][2] ;
+        
+
+        dp[n][0] = 0;
+        dp[n][1] = 0 ;
+
+        for(int i=n-1 ; i>=0 ; i--){
+            for(int buy = 0 ; buy<=1 ; buy++){
+
+                int profit = 0;
+                if(buy==1){
+                    int  ibought = -prices[i] + dp[i+1][0];
+                    profit = Math.max(ibought , 0 + dp[i+1][1]);
+                }
+                else{
+                    int isold = prices[i] + dp[i+1][1];
+                    profit = Math.max(isold , 0+ dp[i+1][0]) ;
+                }
+                dp[i][buy] = profit;
+
+            }
         }
-        return helper(0 , 1 , prices , dp);
+
+
+        return dp[0][1];
     }
 
     public int helper(int i , int buy , int[] prices , int[][] dp){
