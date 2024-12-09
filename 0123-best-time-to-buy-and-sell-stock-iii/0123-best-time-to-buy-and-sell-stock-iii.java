4711127154963f2a307bba416c76c60da221ce80
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-12-09 20:32:37 +0530

    Time: 7 ms (55.43%), Space: 61.3 MB (55.02%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int dp[][][] = new int[n+1][2][3];
+        int dp[][] = new int[2][3];
 
         // for(int[][] d:dp){
         //     for(int[] p : d){
@@ -18,6 +18,8 @@ class Solution {
         //     dp[i][1][0] = 0;
 
         // }
+
+
         
         for(int i = n-1 ; i>= 0 ; i-- ){
             for(int buy = 0 ; buy<=1 ; buy++){
@@ -25,49 +27,49 @@ class Solution {
 
                     int profit = 0;
                     if(buy==1){
-                        int ibought = -prices[i] + dp[i+1][0][cap];
-                        profit = Math.max(ibought , 0 + dp[i+1][1][cap]);
+                        int ibought = -prices[i] + dp[0][cap];
+                        profit = Math.max(ibought , 0 + dp[1][cap]);
                     }
                     else{
-                        int isold = prices[i] + dp[i+1][1][cap-1];
-                        profit = Math.max(isold , 0+dp[i+1][0][cap]);
+                        int isold = prices[i] + dp[1][cap-1];
+                        profit = Math.max(isold , 0+dp[0][cap]);
                     }
 
-                    dp[i][buy][cap] = profit;
+                    dp[buy][cap] = profit;
 
                 }
             }
         }
 
-        return dp[0][1][2] ;
+        return dp[1][2] ;
 
     }
 
-    public int helper(int i , int buy , int cap, int[] prices , int[][][] dp){
+    // public int helper(int i , int buy , int cap, int[] prices , int[][][] dp){
         
-        if(cap==0){
-            return 0;
-        }
+    //     if(cap==0){
+    //         return 0;
+    //     }
 
-        if(i== prices.length){
-            return 0;
-        }
+    //     if(i== prices.length){
+    //         return 0;
+    //     }
 
-        if(dp[i][buy][cap] != -1){
-            return dp[i][buy][cap] ;
-        }
+    //     if(dp[i][buy][cap] != -1){
+    //         return dp[i][buy][cap] ;
+    //     }
 
-        int profit = 0;
-        if(buy==1){
-            int ibought = -prices[i] + helper(i+1 , 0 , cap , prices , dp);
-            profit = Math.max(ibought , 0 + helper(i+1 , 1 , cap , prices , dp));
-        }
-        else{
-            int isold = prices[i] + helper(i+1 , 1 , cap-1 , prices , dp);
-            profit = Math.max(isold , 0+helper(i+1 , 0 , cap , prices , dp));
-        }
+    //     int profit = 0;
+    //     if(buy==1){
+    //         int ibought = -prices[i] + helper(i+1 , 0 , cap , prices , dp);
+    //         profit = Math.max(ibought , 0 + helper(i+1 , 1 , cap , prices , dp));
+    //     }
+    //     else{
+    //         int isold = prices[i] + helper(i+1 , 1 , cap-1 , prices , dp);
+    //         profit = Math.max(isold , 0+helper(i+1 , 0 , cap , prices , dp));
+    //     }
 
-        return dp[i][buy][cap] = profit;
+    //     return dp[i][buy][cap] = profit;
 
-    }
+    // }
 }
