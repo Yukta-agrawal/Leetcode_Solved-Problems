9e73ecb1b4a6d3eecf37d60ff0fb3cfba5ad7ff4
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-12-09 20:23:06 +0530

    Time: 63 ms (44.87%), Space: 63.9 MB (34.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -3,27 +3,43 @@ class Solution {
         int n = prices.length;
         int dp[][][] = new int[n+1][2][3];
 
-        for(int[][] d:dp){
-            for(int[] p : d){
-                Arrays.fill(p , -1);
-            }
-        }
-        // for(int cap = 0 ; cap<=2 ; cap++){
-        //     dp[n][0][cap] = 0 ;
-        //     dp[n][1][cap] = 0
-        // }
-        
-        // for(int i = n-1 ; i>= 0 ; i-- ){
-        //     for(int buy = 0 ; buy<=1 ; buy++){
-        //         for(int cap = 0 ; cap<=2 ; cap++){
-
-
-
-        //         }
+        // for(int[][] d:dp){
+        //     for(int[] p : d){
+        //         Arrays.fill(p , -1);
         //     }
         // }
+        for(int cap = 0 ; cap<=2 ; cap++){
+            dp[n][0][cap] = 0 ;
+            dp[n][1][cap] = 0 ;
+        }
 
-        return helper(0 , 1 , 2 , prices , dp);
+        for(int i=0 ; i<=n  ; i++){
+            dp[i][0][0] = 0;
+            dp[i][1][0] = 0;
+
+        }
+        
+        for(int i = n-1 ; i>= 0 ; i-- ){
+            for(int buy = 0 ; buy<=1 ; buy++){
+                for(int cap =1 ; cap<=2 ; cap++){
+
+                    int profit = 0;
+                    if(buy==1){
+                        int ibought = -prices[i] + dp[i+1][0][cap];
+                        profit = Math.max(ibought , 0 + dp[i+1][1][cap]);
+                    }
+                    else{
+                        int isold = prices[i] + dp[i+1][1][cap-1];
+                        profit = Math.max(isold , 0+dp[i+1][0][cap]);
+                    }
+
+                    dp[i][buy][cap] = profit;
+
+                }
+            }
+        }
+
+        return dp[0][1][2] ;
 
     }
 
