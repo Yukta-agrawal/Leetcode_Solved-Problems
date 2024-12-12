9e1c5c23364a1949f9d18f2c52f0e86c3564a9b3
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-12-12 19:41:04 +0530

    Time: 6 ms (63.67%), Space: 55.2 MB (58.71%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,52 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         
-        int n = prices.length;
+      int n = prices.length;
       int[] dp = new int[2];
     
         for(int i=n-1 ; i>=0 ; i--){
-            for(int buy = 0 ; buy<=1 ; buy++){
 
-                int profit = 0;
-                if(buy==1){
                     int  ibought = -prices[i] + dp[0];
-                    profit = Math.max(ibought , 0 + dp[1]);
-                }
-                else{
+                    dp[1] = Math.max(ibought , 0 + dp[1]);
+                
                     int isold = prices[i] - fee + dp[1];
-                    profit = Math.max(isold , 0+ dp[0]) ;
-                }
-                dp[buy] = profit;
-
-            }
+                    dp[0] = Math.max(isold , 0+ dp[0]) ;
         }
 
 
         return dp[1];
-    }
-
-    public int helper(int i , int buy , int[] prices , int[][] dp , int fee){
-
-        if(i== prices.length){
-            return 0;
-        }
-
-        if(dp[i][buy]!= -1){
-            return dp[i][buy]; 
-        }
-
-        int profit = 0;
-        if(buy==1){
-            int  ibought = -prices[i] + helper(i+1 , 0 , prices , dp , fee);
-            profit = Math.max(ibought , 0 + helper(i+1 , 1 , prices , dp , fee));
-        }
-        else{
-            int isold = prices[i] - fee + helper(i+1 , 1 , prices , dp , fee);
-            profit = Math.max(isold , 0+helper(i+1 , 0 , prices , dp , fee) ) ;
-        }
-
-        return dp[i][buy] = profit;
-
     }
 
 }
